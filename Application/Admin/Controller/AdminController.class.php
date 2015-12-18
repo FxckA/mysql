130e23bd06cec78a836c2fec4e3f5b085fba53cd
phantomer <rootphantomy@gmail.com>
2015-12-18 21:21:11 +0800

    update

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -3,6 +3,13 @@ namespace Admin\Controller;
 use Think\Controller;
 class AdminController extends Controller {
     public function index(){
-		$this -> display(T('login'));
+		$this -> display(T('login'));//展示登陆页面
     }
+	public function login($username = null ,$password = null ){//接收post数据
+		if(IS_POST){
+			echo $username.'</br>';
+			echo $password.'</br>';
+		}
+		
+	}
 }
