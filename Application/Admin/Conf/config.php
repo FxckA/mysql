3caec8f0ce7abcc4653aa6e5c7a2cc693468f023
phantomer <rootphantomy@gmail.com>
2015-12-20 01:29:38 +0800

    模块继承index完成

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -3,5 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	'DEFAULT_CONTROLLER'    =>  'Login',// 默认控制器名称
 	'view_filter' => array('Behavior\TokenBuildBehavior'),
+	'TEMPLATE_CHARSET' =>'utf-8',
+	'OUTPUT_CHARSET' =>'utf-8',
 	
 );
