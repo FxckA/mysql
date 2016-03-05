12e2db403e54a3e98a6809f34dd95354e3f8c72e
phantomer <rootphantomy@gmail.com>
2016-03-05 17:47:11 +0800

    添加了一些注释

--- Application/Admin/Controller/ArticlescrapController.class.php
+++ Application/Admin/Controller/ArticlescrapController.class.php
@@ -11,8 +11,8 @@ class ArticlescrapController extends BaseController {
 	 */
 	public function index($key = "") {
 		if ($key === "") {
-			$model = D('ArticlescrapView');
-		} else {
+			$model = D('ArticlescrapView');//默认情况下显示全部
+		} else {//搜索功能的时候搜索特定的文章
 			$where['articlescrap.title'] = array('like', "%$key%");
 			$where['member.username'] = array('like', "%$key%");
 			$where['category.title'] = array('like', "%$key%");
--- Application/Admin/Model/ArticlescrapViewModel.class.php
+++ Application/Admin/Model/ArticlescrapViewModel.class.php
@@ -3,7 +3,7 @@ namespace Admin\Model;
 use Think\Model\ViewModel;
 
 class ArticlescrapViewModel extends ViewModel {
-   public $viewFields = array(
+   public $viewFields = array(//展示默认文章的视图模型
      'articlescrap'=>array('id','title','content','user_id','cate_id','time','type'),
      'category'=>array('name'=>'category_name','title'=>'category_title', '_on'=>'articlescrap.cate_id=category.id'),
      'member'=>array('username', '_on'=>'articlescrap.user_id=member.id'),
