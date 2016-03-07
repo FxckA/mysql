ccab273890d1f9d8c01478824d3d838e507a0e33
phantomer <rootphantomy@gmail.com>
2016-03-07 18:25:53 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,10 +4,42 @@ namespace Home\Controller;
 
 use Think\Controller;
 
-class IndexController extends BaseController{
+class IndexController extends BaseController {
 
-    public function index(){
+	public function index() {//首页展示
+		$this -> display();
+	}
 
-        $this->display();
-    }
+	public function led() {//现任领导
+
+	}
+
+	public function synopsis() {//实验室简介
+
+	}
+
+	public function news() {//动态新闻
+
+	}
+
+	public function research() {//科学研究
+
+	}
+
+	public function study() {//学术梯队
+
+	}
+
+	public function education() {//研究生教学
+
+	}
+
+	public function benkestudy() {//本科生教学
+
+	}
+
+	public function callme() {//联系我们
+
+	}
+
 }
